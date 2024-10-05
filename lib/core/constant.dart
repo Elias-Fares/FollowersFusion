a17b735f70a0bafdedac742ce643a25a3f6bf004
Elias Fares <140490818+Elias-Fares@users.noreply.github.com>
2024-10-05 22:32:37 +0300

    Update constant.dart

--- lib/core/constant.dart
+++ lib/core/constant.dart
@@ -9,12 +9,12 @@ const String AR = "ar";
 const String EN = "en";
 
 const TEMP_IMAGE =
-    "https://hips.hearstapps.com/hmg-prod/images/best-skincare-products-1656081764.jpg?crop=1.00xw:0.503xh;0,0.279xh&resize=1200:*";
+    "";
 
 const String PROJECT_API = "project_api";
 const String GOOGLE_API = "google_api";
 const String BaseUrlGoogle = "https://maps.googleapis.com/maps/api/";
-const GoogleToken = "AIzaSyDWasGASHKWQQx7dKevZNbQD7w_SFGBLAE";
+const GoogleToken = "";
 
 const SFPro = 'SfPro';
 const Almarai = 'Almarai';
@@ -29,8 +29,8 @@ const SPLIT_VALUE_VISA_QUESTION = "****";
 
 class Constant {
   static String getBaseUrl =
-      'https://infoapp.bw-businessworld.com/api/projectInfo/HJBGJH678HGJGBVcdxsf';
-  static String baseUrl = 'http://store.alreemsmartkids.com';
+      '';
+  static String baseUrl = '';
   static String mediaUrl = '';
   static String appCurrency = Currencies.sar;
   static const String ACCESS_TOKEN = 'ACCESS_TOKEN';
@@ -47,7 +47,7 @@ class Constant {
 
   //payment method
   static const String myFatorahTestToken =
-      "rLtt6JWvbUHDDhsZnfpAhpYk4dxYDQkbcPTyGaKp2TYqQgG7FGZ5Th_WD53Oq8Ebz6A53njUoo1w3pjU1D4vs_ZMqFiz_j0urb_BH9Oq9VZoKFoJEDAbRZepGcQanImyYrry7Kt6MnMdgfG5jn4HngWoRdKduNNyP4kzcp3mRv7x00ahkm9LAK7ZRieg7k1PDAnBIOG3EyVSJ5kK4WLMvYr7sCwHbHcu4A5WwelxYK0GMJy37bNAarSJDFQsJ2ZvJjvMDmfWwDVFEVe_5tOomfVNt6bOg9mexbGjMrnHBnKnZR1vQbBtQieDlQepzTZMuQrSuKn-t5XZM7V6fCW7oP-uXGX-sMOajeX65JOf6XVpk29DP6ro8WTAflCDANC193yof8-f5_EYY-3hXhJj7RBXmizDpneEQDSaSz5sFk0sV5qPcARJ9zGG73vuGFyenjPPmtDtXtpx35A-BVcOSBYVIWe9kndG3nclfefjKEuZ3m4jL9Gg1h2JBvmXSMYiZtp9MR5I6pvbvylU_PP5xJFSjVTIz7IQSjcVGO41npnwIxRXNRxFOdIUHn0tjQ-7LwvEcTXyPsHXcMD8WtgBh-wxR8aKX7WPSsT1O8d8reb2aR7K3rkV3K82K_0OgawImEpwSvp9MNKynEAJQS6ZHe_J_l77652xwPNxMRTMASk1ZsJL";
+      "";
 
   static const String myFatorahLiveToken ="";
 
